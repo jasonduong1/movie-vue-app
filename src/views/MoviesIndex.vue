800217f5c44142fc29a48b500c736568c084bdbb
jason <jduong89@gmail.com>
2022-04-13 19:50:00 -0700

    adds movies index action/route

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/movies.json").then((response) => {
+      console.log("movies", response.data);
+      this.movies = response.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <div v-for="movie in movies" :key="movie">
+    <p>{{ movie }}</p>
+  </div>
+</template>
