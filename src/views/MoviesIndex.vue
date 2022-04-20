86972500e0c8d3904758873c1a8b50a9987c5c78
jason <jduong89@gmail.com>
2022-04-19 21:52:02 -0700

    drys up sort code

--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -26,13 +26,7 @@ export default {
       sortedMovies = sortedMovies.sort((a, b) => {
         let fa = a.title.toLowerCase(),
           fb = b.title.toLowerCase();
-        if (fa < fb) {
-          return -1;
-        }
-        if (fa > fb) {
-          return 1;
-        }
-        return 0;
+        return fa == fb ? 0 : fa > fb ? 1 : -1;
       });
       return sortedMovies;
     },
